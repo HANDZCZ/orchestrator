9f775198c68467d6744c550385beee94bdd52583
HANDZCZ <handz@email.cz>
2024-07-01 23:07:38 +0200

    Add docs to converting GenericOrchestrator into pipeline

--- src/generic/orchestrator/generic_orchestrator_as_pipeline.rs
+++ src/generic/orchestrator/generic_orchestrator_as_pipeline.rs
@@ -6,12 +6,105 @@ use crate::{generic::pipeline::PipelineOutput, pipeline::Pipeline};
 
 use super::{ErrorInner, GenericOrchestrator};
 
+/// Type that wraps around [`GenericOrchestrator`] to crate a [`Pipeline`] from it.
+///
+/// This type correctly converts [`AllPipelinesSoftFailed`](crate::generic::orchestrator::OrchestratorError::AllPipelinesSoftFailed) error into [`PipelineOutput::SoftFail`].
+///
+/// Example that shows usage of [`GenericOrchestratorAsPipeline`].
+/// ```
+/// #[derive(Debug, PartialEq)]
+/// enum MyOrchestratorError<T> {
+///     AllPipelinesSoftFailed,
+///     PipelineError(T),
+/// }
+/// #
+/// // conversion from generic orchestrator error into ours
+/// impl<T> From<OrchestratorError> for MyOrchestratorError<T> //...
+/// # {
+/// #     fn from(_value: orchestrator::generic::orchestrator::OrchestratorError) -> Self {
+/// #         Self::AllPipelinesSoftFailed
+/// #     }
+/// # }
+/// #
+/// // conversion from ForwardPipeline error into ours
+/// impl From<()> for MyOrchestratorError<()> //...
+/// # {
+/// #     fn from(_value: ()) -> Self {
+/// #         Self::PipelineError(())
+/// #     }
+/// # }
+///
+/// // some pipeline implementation
+/// #[derive(Default, Debug, Clone)]
+/// struct ForwardPipeline<T: Default> {
+///     _type: PhantomData<T>,
+///     // tells us how many times the pipeline ran
+///     ran: Arc<RwLock<usize>>,
+/// }
+/// #
+/// #[async_trait]
+/// impl<T: Send + Sync + 'static + Default> Pipeline for ForwardPipeline<T> {
+///     type Input = T;
+///     type Output = PipelineOutput<T>;
+///     type Error = ();
+///
+///     async fn run(&self, input: Self::Input) -> Result<Self::Output, Self::Error> {
+///         let mut ran = self.ran.write().unwrap();
+///         *ran += 1;
+///         // here you want to actually do something like running some nodes
+///         // but we will just forward input to output
+///         Ok(PipelineOutput::Done(input))
+///     }
+/// }
+///
+/// # use orchestrator::{async_trait, pipeline::Pipeline, orchestrator::Orchestrator, generic::{pipeline::PipelineOutput, orchestrator::{GenericOrchestrator, OrchestratorError}}};
+/// # use std::marker::PhantomData;
+/// # use std::sync::{RwLock, Arc};
+/// #[tokio::main]
+/// async fn main() {
+///     let input = "Nice".to_owned();
+///
+///     // construct generic orchestrator that takes and returns a string
+///     let mut orchestrator = GenericOrchestrator::<String, String, MyOrchestratorError<()>>::new();
+///     // construct and add a pipeline that takes and returns a String
+///     let string_pipeline = ForwardPipeline::<String>::default();
+///     orchestrator.add_pipeline(string_pipeline.clone());
+///     orchestrator.add_pipeline(string_pipeline.clone());
+///     orchestrator.add_pipeline(string_pipeline.clone());
+///
+///     // convert orchestrator into pipeline
+///     let orchestrator_as_pipeline = orchestrator.into_pipeline();
+///
+///     // construct generic orchestrator that takes and returns a string
+///     let mut orchestrator = GenericOrchestrator::<String, String, MyOrchestratorError<()>>::new();
+///     // construct and add an orchestrator as a pipeline that takes and returns a String
+///     // this but orchestrator doesn't have any pipelines so it returns an error AllPipelinesSoftFailed
+///     let empty_orchestrator = GenericOrchestrator::<String, String, MyOrchestratorError<()>>::new();
+///     orchestrator.add_pipeline(empty_orchestrator.into_pipeline());
+///     // add a pipeline that was created from orchestrator
+///     orchestrator.add_pipeline(orchestrator_as_pipeline);
+///
+///     // run the orchestrator
+///     let output = orchestrator.run(input.clone()).await;
+///
+///     // output should be the same as input
+///     // since the error from the empty orchestrator is AllPipelinesSoftFailed
+///     // this error is simply converted into PipelineOutput::SoftFail
+///     // that means that the second pipeline is ran and that one returns successfully
+///     assert_eq!(output, Ok(input));
+///
+///     // pipeline should have ran only once
+///     let ran = string_pipeline.ran.read().unwrap();
+///     assert_eq!(*ran, 1);
+/// }
+/// ```
 #[derive(Debug)]
 pub struct GenericOrchestratorAsPipeline<Input, Output, Error> {
     orchestrator: Arc<GenericOrchestrator<Input, Output, Error>>,
 }
 
 impl<Input, Output, Error> GenericOrchestratorAsPipeline<Input, Output, Error> {
+    /// Creates new instance of [`GenericOrchestratorAsPipeline`] from [`GenericOrchestrator`].
     #[must_use]
     pub fn new(orchestrator: GenericOrchestrator<Input, Output, Error>) -> Self {
         Self {
@@ -28,6 +121,7 @@ impl<Input, Output, Error> From<GenericOrchestrator<Input, Output, Error>>
 }
 
 impl<Input, Output, Error> GenericOrchestrator<Input, Output, Error> {
+    /// Converts [`GenericOrchestrator`] into [`Pipeline`].
     #[must_use]
     pub fn into_pipeline(self) -> GenericOrchestratorAsPipeline<Input, Output, Error> {
         GenericOrchestratorAsPipeline::new(self)
